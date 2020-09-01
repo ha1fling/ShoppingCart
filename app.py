452e42297967e07b68fc5182e34ed6b1f15c60c4
Jenny <jenny@cyberfount.com>
2020-09-01 18:04:37 +0100

    created a JSON array of items and created a list of python objects from this. The front end loops through the python list, displaying the name and cost of each item in the list

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from items import data
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def display_index():
-    return render_template('index.html')
+
+    return render_template('index.html', items=data)
